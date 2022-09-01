fc201b600432078e3b8fdd40e69bde6ba9ce2df3
Benjamin Legrand <benjilegnard@protonmail.com>
2022-09-01 17:33:11 +0200

    feat: add gamepad support 🎮

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
-import 'reveal.js/plugin/highlight/monokai.css';
-import 'reveal.js/plugin/highlight/highlight.esm';
+import "reveal.js/plugin/highlight/monokai.css";
+import "reveal.js/plugin/highlight/highlight.esm";
 
-import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-import Notes from 'reveal.js/plugin/notes/notes.esm.js';
-import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import Notes from "reveal.js/plugin/notes/notes.esm.js";
+import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
 
+import Gamepad from "./plugin/gamepad";
 
 let deck = new Reveal({
-   plugins: [ Markdown, Notes, Highlight ]
+	plugins: [
+		Markdown,
+		Notes,
+		Highlight,
+		Gamepad({
+			threshold: 0.85,
+			debounceTime: 500,
+			invertAxis: false,
+			rotateAxis: true,
+		}),
+	],
 });
 
 deck.initialize({
-   progress: false,
-   controls: false,
-   slideNumber: "c/t",
-   showSlideNumber: 'all',
-   hashOneBasedIndex: true,
-   hash: true,
-   history: true,
+	progress: false,
+	controls: false,
+	slideNumber: "c/t",
+	showSlideNumber: "all",
+	hashOneBasedIndex: true,
+	hash: true,
+	history: true,
 });
